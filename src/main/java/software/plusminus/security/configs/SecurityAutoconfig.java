8617589e44274627ca6b1e0a6dba47ea3d939b89
Taras Shpek <taras.shpek@gmail.com>
2022-07-31 13:49:02 +0300

    remove @EnableJpaAuditing from SecurityAutoconfig

--- src/main/java/software/plusminus/security/configs/SecurityAutoconfig.java
+++ src/main/java/software/plusminus/security/configs/SecurityAutoconfig.java
@@ -1,20 +1,10 @@
 package software.plusminus.security.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @Configuration
 @ComponentScan("software.plusminus.security")
 public class SecurityAutoconfig {
-    
-    @Bean
-    public AuditorAware<String> auditorProvider() {
-        return new SecurityAuditorAware();
-    }
-    
 }
 
--- src/test/java/software/plusminus/security/TestApplication.java
+++ src/test/java/software/plusminus/security/TestApplication.java
@@ -3,14 +3,24 @@ package software.plusminus.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import software.plusminus.security.configs.SecurityAuditorAware;
 import software.plusminus.security.configs.SecurityAutoconfig;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 @EntityScan("software.plusminus.security")
 @Import(SecurityAutoconfig.class)
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new SecurityAuditorAware();
     }
 }
