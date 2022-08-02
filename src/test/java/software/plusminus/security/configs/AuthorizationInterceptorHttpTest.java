4382bc34dbce62057b71a5ce56a74c2390a947c9
Taras Shpek <taras.shpek@gmail.com>
2022-08-02 16:55:37 +0300

    update test on /login page

--- src/test/java/software/plusminus/security/configs/AuthorizationInterceptorHttpTest.java
+++ src/test/java/software/plusminus/security/configs/AuthorizationInterceptorHttpTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import software.plusminus.jwt.service.JwtGenerator;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@TestPropertySource(properties = "security.loginPage=/login")
 public class AuthorizationInterceptorHttpTest {
 
     @Autowired
