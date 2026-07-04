66503556f972c62c5e807e1b11bef5a930a9bb11
Taras Shpek <taras.shpek@gmail.com>
2026-07-04 23:43:04 +0300

    Update dependencies and refactor tests replacing Selenium with Browser

--- plusminus-login/src/test/java/software/plusminus/login/controller/LoginControllerSeleniumTest.java
+++ plusminus-login/src/test/java/software/plusminus/login/controller/LoginControllerSeleniumTest.java
@@ -6,20 +6,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import software.plusminus.browser.Browser;
+import software.plusminus.browser.BrowserSettings;
+import software.plusminus.browser.Page;
 import software.plusminus.security.Security;
 import software.plusminus.security.service.CredentialService;
 import software.plusminus.security.service.TokenProcessor;
-import software.plusminus.selenium.Findable;
-import software.plusminus.selenium.Finder;
-import software.plusminus.selenium.Selenium;
-import software.plusminus.selenium.model.SeleniumOptions;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-class LoginControllerSeleniumTest implements Findable {
+class LoginControllerSeleniumTest {
 
     @LocalServerPort
     private int port;
@@ -32,67 +31,59 @@ class LoginControllerSeleniumTest implements Findable {
     private String password = "test-password";
     private Security security = Security.builder().username(email).build();
     private String token = "test-token";
-    private Selenium selenium = new Selenium();
-    private SeleniumOptions options = new SeleniumOptions()
-            .logsFilter(logEntry -> !logEntry.getMessage().contains("404")
-                    && !logEntry.getMessage().contains("favicon.ico"));
+    private Browser browser;
 
     @BeforeEach
     void beforeEach() {
+        BrowserSettings browserSettings = new BrowserSettings()
+                .port(port)
+                .logsFilter(logEntry -> !logEntry.contains("404") && !logEntry.contains("favicon.ico"));
+        browser = Browser.create(browserSettings);
         when(credentialService.provideSecurity(email, password)).thenReturn(security);
         when(tokenProcessor.getToken(security)).thenReturn(token);
         when(tokenProcessor.getSecurity(token)).thenReturn(security);
-        selenium.openBrowser(options);
-        selenium.driver().manage().deleteAllCookies();
-    }
-
-    @Override
-    public Finder find() {
-        return new Finder(selenium);
+        browser.open();
+        browser.cookies().clear();
     }
 
     @Test
     void successfulLogin() {
-        selenium.loadPage(options, url("/login"));
+        Page page = browser.go("/login");
 
-        find("#email").one().sendKeys(email);
-        find("#password").one().sendKeys(password);
-        find("#submit").one().click();
-        
-        find().byText("div", "Logged in").one();
+        page.find("#email").one().sendKeys(email);
+        page.find("#password").one().sendKeys(password);
+        page.find("#submit").one().click();
+
+        browser.currentPage().find().byText("div", "Logged in").one();
     }
 
     @Test
     void badCredentials() {
-        selenium.loadPage(options, url("/login"));
+        Page page = browser.go("/login");
 
-        find("#email").one().sendKeys("bad@email.com");
-        find("#password").one().sendKeys("bad-password");
-        find("#submit").one().click();
+        page.find("#email").one().sendKeys("bad@email.com");
+        page.find("#password").one().sendKeys("bad-password");
+        page.find("#submit").one().click();
 
-        find().byText("div", "Invalid username or password!").one();
+        page.find().byText("div", "Invalid username or password!").one();
     }
 
     @Test
     void indexPageIsNotPublic() {
-        selenium.loadPage(options, url("/"));
-        String body = find("body").one().getText();
+        Page page = browser.go("/");
+        String body = page.find("body").one().text();
         assertThat(body).doesNotContain("Index page")
                 .contains("Not Found");
     }
     
     @Test
     void explicitRedirect() {
-        selenium.loadPage(options, url("/login?redirect=explicit-redirect"));
+        Page page = browser.go("/login?redirect=explicit-redirect");
 
-        find("#email").one().sendKeys(email);
-        find("#password").one().sendKeys(password);
-        find("#submit").one().click();
+        page.find("#email").one().sendKeys(email);
+        page.find("#password").one().sendKeys(password);
+        page.find("#submit").one().click();
         
-        assertThat(selenium.driver().getCurrentUrl()).endsWith("explicit-redirect");
-    }
-
-    private String url(String page) {
-        return "http://localhost:" + port + page;
+        assertThat(browser.url()).endsWith("explicit-redirect");
     }
 }
--- plusminus-login/src/test/java/software/plusminus/login/controller/LogoutControllerSeleniumTest.java
+++ plusminus-login/src/test/java/software/plusminus/login/controller/LogoutControllerSeleniumTest.java
@@ -2,25 +2,23 @@ package software.plusminus.login.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Cookie;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import software.plusminus.authentication.service.token.HttpTokenContext;
+import software.plusminus.browser.Browser;
+import software.plusminus.browser.BrowserSettings;
+import software.plusminus.browser.Page;
 import software.plusminus.security.Security;
 import software.plusminus.security.service.TokenProcessor;
-import software.plusminus.selenium.Findable;
-import software.plusminus.selenium.Finder;
-import software.plusminus.selenium.Selenium;
-import software.plusminus.selenium.model.SeleniumOptions;
 
 import static org.mockito.Mockito.when;
 import static software.plusminus.check.Checks.check;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-public class LogoutControllerSeleniumTest implements Findable {
+class LogoutControllerSeleniumTest {
 
     @LocalServerPort
     private int port;
@@ -30,38 +28,35 @@ public class LogoutControllerSeleniumTest implements Findable {
     private String token = "test-token";
     private String email = "test@email.com";
     private Security security = Security.builder().username(email).build();
-    private Selenium selenium = new Selenium();
-    private SeleniumOptions options = new SeleniumOptions()
-            .logsFilter(logEntry -> !logEntry.getMessage().contains("404")
-                    && !logEntry.getMessage().contains("favicon.ico"));
+    private Browser browser;
+    private Page page;
 
     @BeforeEach
     void beforeEach() {
+        BrowserSettings browserSettings = new BrowserSettings()
+                .port(port)
+                .logsFilter(logEntry -> !logEntry.contains("404") && !logEntry.contains("favicon.ico"));
+        browser = Browser.create(browserSettings);
         when(tokenProcessor.getSecurity(token)).thenReturn(security);
-        selenium.openBrowser(options);
-        selenium.loadPage(options, "http://localhost:" + port);
-        selenium.driver().manage().addCookie(new Cookie(HttpTokenContext.COOKIE_NAME, token));
-        selenium.driver().navigate().refresh();
-    }
-
-    @Override
-    public Finder find() {
-        return new Finder(selenium);
+        browser.open();
+        browser.go("/");
+        browser.cookies().add(HttpTokenContext.COOKIE_NAME, token);
+        page = browser.refresh();
     }
 
     @Test
     void logoutClearsCookies() {
-        find("#logout").one().click();
-        find("#logout").none();
+        page.find("#logout").one().click();
+        page.find("#logout").none();
 
-        check(selenium.driver().manage().getCookies()).isEmpty();
+        check(browser.cookies().getAll()).isEmpty();
     }
 
     @Test
     void logoutRedirectsToIndexPage() {
-        find("#logout").one().click();
-        find("#logout").none();
+        page.find("#logout").one().click();
+        page.find("#logout").none();
 
-        check(selenium.driver().getCurrentUrl()).is("http://localhost:" + port + "/");
+        check(browser.url()).is("http://localhost:" + port + "/");
     }
 }
